c4f51bde29df7b71d539df124207e83223904ff8
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-09-10 18:16:49 -0300

    fix: database.js and accept mysqlurl and information of db

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = mysql.createPool(
-    process.env.MYSQL_URL
-);
+const pool = process.env.MYSQL_URL
+    ? mysql.createPool(process.env.MYSQL_URL)
+    : mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    });
 
 export default pool;
